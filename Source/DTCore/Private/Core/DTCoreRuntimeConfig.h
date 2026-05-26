b6fe18e088f4b467479f4eaba98f19946bcc12d3
YonghoKwon <ho2708@naver.com>
2026-05-26 10:57:15 +0900

    Remove private runtime config helper header

--- Source/DTCore/Private/Core/DTCoreRuntimeConfig.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-#include "CoreMinimal.h"
-
-namespace DTCoreRuntimeConfig
-{
-	bool TryReadRuntimeOverride(const TCHAR* Key, FString& OutValue);
-	void EnsureRuntimeOverrideTemplate();
-}
